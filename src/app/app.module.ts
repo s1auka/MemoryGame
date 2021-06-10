5f8b55ec8315db04e824a36b2a02daa85867928e
Svyatoslav <s1aukaa@gmail.com>
2021-06-10 17:04:51 +0300

    add initial content-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
+        ContentComponent,
     ],
     imports: [
         BrowserModule,
