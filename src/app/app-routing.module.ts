9716e203ba960ec8a33b1e9504add43632a7036d
Svyatoslav <s1aukaa@gmail.com>
2021-06-10 18:07:30 +0300

    routing refact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContentComponent } from './components/content/content.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+
+const routes: Routes = [
+    { path: '', component: ContentComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: 'rules', component: RulesComponent },
+    { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ContentComponent } from './components/content/content.component';
-import { RulesComponent } from './components/rules/rules.component';
-import { SettingsComponent } from './components/settings/settings.component';
 
-const appRoutes: Routes = [
-    { path: '', component: ContentComponent },
-    { path: 'settings', component: SettingsComponent },
-    { path: 'rules', component: RulesComponent },
-    { path: '**', redirectTo: '/' }
-
-];
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
-        ContentComponent,
-        RulesComponent,
-        SettingsComponent,
     ],
     imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes)
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
