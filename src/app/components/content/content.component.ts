c23a20f8c27a42b17febc22e5c94c00a37c8d012
Svyatoslav <s1aukaa@gmail.com>
2021-06-21 03:41:22 +0300

    clickable images

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,6 +1,5 @@
-import { CardsInTable, ItemControlService } from './../../services/settings.service';
+import { ItemControlService } from './../../services/settings.service';
 import { Component } from '@angular/core';
-import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/template';
 
 @Component({
     selector: 'app-content',
@@ -8,15 +7,19 @@ import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/temp
     styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-    items: any | undefined;
     gameOn: boolean;
-    cardsInTable: CardsInTable | undefined;
     columns: number[] | undefined;
-    rows: number[] | undefined;
-    imagesSrcs: (string | number)[][] | undefined;
+    rows: number[] = [];
+    imagesSrcs: (string | number)[][] = [[]];
+    cardBack: string;
+    firstOpenedCard: any = undefined;
+    secondOpenedCard: any = undefined;
+    cells: number[] = [];
+
 
     constructor(private itemControlService: ItemControlService) {
         this.gameOn = false;
+        this.cardBack = "./../../../assets/pictures/cardBack.jpg"
     }
 
     private toggleGameStatus() {
@@ -24,25 +27,39 @@ export class ContentComponent {
     }
 
     startGame() {
-        this.toggleGameStatus()
-        this.cardsInTable = this.itemControlService.getCardsInRowAndColumn();
-        this.columns = [...Array(this.cardsInTable.cardsInColumn).keys()];
-        this.rows = [...Array(this.cardsInTable.cardsInRow).keys()];
-        this.imagesSrcs = this.getImagesSrcs(+this.cardsInTable.totalCards);
+        this.toggleGameStatus();
+        let cardsInTable = this.itemControlService.getCardsInRowAndColumn();
 
+
+        this.columns = [...Array(cardsInTable.cardsInColumn).keys()];
+        this.rows = [...Array(cardsInTable.cardsInRow).keys()];
+        this.imagesSrcs = this.getImagesSrcs(+cardsInTable.totalCards);
+        this.cells = [...Array(cardsInTable.totalCards).keys()]
     }
 
     private getImagesSrcs(n: number): (string | number)[][] {
         let array = [];
         let imgSrc: string;
+
         for (let i = 0; i < n / 2; i++) {
             imgSrc = "./../../../assets/pictures/" + i + ".png";
             array.push([imgSrc, i]);
             array.push([imgSrc, i]);
+
         }
         return this.shuffle(array);
     }
 
+    checkCard(event: any) {
+        let elem = event.target;
+        let numberOfPicture = elem.attributes["cell-number"].value;
+        let pictureSrc = "url(" + this.imagesSrcs[+numberOfPicture][0] + ")";
+
+        console.log(numberOfPicture);
+        elem.style.backgroundImage = pictureSrc;
+
+        this.firstOpenedCard ? this.firstOpenedCard = event.target : this.secondOpenedCard = event.target;
+    }
 
     private shuffle(array: (string | number)[][]): (string | number)[][] {
         for (let i = array.length - 1; i > 0; i--) {
@@ -53,6 +70,8 @@ export class ContentComponent {
     }
 
     stopGame() {
-        this.toggleGameStatus()
+        this.toggleGameStatus();
+        window.location.reload();
     }
+
 }
